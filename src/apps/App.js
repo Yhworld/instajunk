05d25d4032f8e85fc2fc09b39026c64350137813
Yahya <yahyahussein670@gmail.com>
2022-09-07 17:26:36 +0300

    import the browser router package from reacr-router-dom, Add a signup page url path

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import SignUp from './SignUp'
 import { Container } from 'react-bootstrap'
+import  { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className='w-100' style={{ maxWidth: "370px" }}>
-        <SignUp />
+        <Router>
+        <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        </Router>
       </div>
     </Container>
   )
