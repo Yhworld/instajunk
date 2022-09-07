ae4472171812a19d60f39af04efd172e63601396
Yahya <yahyahussein670@gmail.com>
2022-09-08 01:15:56 +0300

    wrap the Userprovider context over the apps components, add a sign in route path which will enable users to shift from the sign up page  to the login if they have an account in place already

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import SignUp from './SignUp'
+import SignIn from './SignIn'
 import { Container } from 'react-bootstrap'
 import  { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { UserProvider } from './context/Authentication'
 
 function App() {
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className='w-100' style={{ maxWidth: "370px" }}>
+      <UserProvider>
         <Router>
         <Routes>
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
         </Routes>
         </Router>
+      </UserProvider>
       </div>
     </Container>
   )
