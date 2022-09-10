ba770a2e2a4942f7fc46fa251d8317b4b826aafe
Yahya <yahyahussein670@gmail.com>
2022-09-10 11:28:31 +0300

    update the useState, add a post method to post to the server and validate the password and email

--- src/apps/SignIn.js
+++ src/apps/SignIn.js
@@ -1,25 +1,65 @@
-import React, { useState }  from 'react'
-import { Form, Card, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext }  from 'react'
+import { Form, Card, Button, Container } from 'react-bootstrap'
+import { useNavigate, Link } from "react-router-dom";
+import { UserContext } from './context/Authentication';
 
 function SignIn() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const {setUser} = useContext(UserContext);
+    
+    const [userProperties, setUserProperties] = useState({
+        username: "",
+        password: ""
+       
+    });
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setUserProperties({
+            ...userProperties,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+
+    fetch("http://localhost:9292/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userProperties)
+    })
+    .then(resp => {
+        if (resp.status === 200) {
+        resp.json()
+        .then((data) => {
+            setUser(data.user)
+            navigate("/")
+        })
+    }else {
+        resp.json()
+        .then(data => console.log(data.error))
+    }
+    })
+    .catch(err => console.log(err))
+}
 
   return (
       <>
+     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <div className='w-100' style={{ maxWidth: "370px" }}>
       <Card>
           <Card.Body>
               <h2 id='logo'>Instajunk</h2>
               <br />
-              <Form>
-                  <Form.Group id="email">
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group id="username">
+                      <Form.Label>username</Form.Label>
+                      <Form.Control name="username" type="text" value={userProperties.username} onChange={handleChange} required/>
                   </Form.Group>
                   <Form.Group id="password">
                       <Form.Label>password</Form.Label>
-                      <Form.Control type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                      <Form.Control name="password" type="password" value={userProperties.password} onChange={handleChange} required/>
                   </Form.Group>
                   <br />
                   <Button className='w-100' type="Log in">Sign Up</Button>
@@ -29,6 +69,8 @@ function SignIn() {
       <div className="w-100 text-center mt-2">
          Dont have an account? <Link to="/signup">Log in</Link>
       </div>
+      </div>
+      </Container>
       </>
   )
 }
