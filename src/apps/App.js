a2a3840eecee156d7cf8ed6957740670c405927e
Yahya <yahyahussein670@gmail.com>
2022-09-10 11:55:08 +0300

    import Homepage.js and changes its route path to / making it the default page

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import SignUp from './SignUp'
 import SignIn from './SignIn'
-import { Container } from 'react-bootstrap'
 import  { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { UserProvider } from './context/Authentication'
+import Homepage from './Homepage'
+import AddPost from './AddPost'
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className='w-100' style={{ maxWidth: "370px" }}>
       <UserProvider>
         <Router>
         <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/addpost" element={<AddPost />} />
         </Routes>
         </Router>
       </UserProvider>
-      </div>
-    </Container>
   )
 }
 
